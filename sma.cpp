83afcd321708cf6b3416516929c0a66ff5aa95c1
= <lucasparza@gmail.com>
2017-05-22 09:54:36 -0300

    Semaphore fixed for writings.

--- sma.cpp
+++ sma.cpp
@@ -19,11 +19,22 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
+// Semaphore
+#include <sys/sem.h>
+#include "semun.h"
+
 // Suppressing a few warnings
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
 
 using namespace std;
+
+static int set_semvalue(void);
+static void del_semvalue(void);
+static int semaphore_p(void);   // para esperar pelo acesso
+static int semaphore_v(void);   // para liberar o acesso
+
+static int sem_id;
 
 // SEND MESSAGE TO SERVERS
 void SMA::send(string address, SHMessage msg) {
@@ -197,6 +208,19 @@ void SMA::byteMe(size_t sharedSize) {
 // Save nbytes [1,k] bytes from ptr to shared memory starting at localStartPos [0,k-1]
 void SMA::save(const char * ptr, int localStartPos, int nbytes) {
     char *s;
+    sem_id = semget((key_t)1234, 1, 0666 | IPC_CREAT);
+
+    // LOCK
+    if (!set_semvalue()) {
+        fprintf(stderr, "Falha ao inicializar o semaforo\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (!semaphore_p()) {
+        exit(EXIT_FAILURE);
+    }
+    log("\tLocked");
+
     if(shmsz < localStartPos+nbytes){
         log("ERROR @SMA::save() >>> Trying to fit "+to_string(localStartPos+nbytes)+"b in "+to_string(shmsz)+"b space.");
         exit(3);
@@ -206,7 +230,15 @@ void SMA::save(const char * ptr, int localStartPos, int nbytes) {
         *s = *ptr;
         ptr++;
     }
-*s = '\0';                  // force null termination
+
+    *s = '\0';                  // force null termination
+
+    // UNLOCK
+    if (!semaphore_v())
+        exit(EXIT_FAILURE);
+    log("\tUnlocked");
+    del_semvalue();
+
     logMemory();
 }
 
@@ -243,6 +275,62 @@ void SMA::logMemory() {
         if(*s == '\0') printf(".");
     }
     printf("\n");
+}
+
+/*
+set_semvalue inicializa o semaforo usando o comando SETVAL em uma chamada semctl.
+*/
+
+static int set_semvalue(void)  {
+    union semun sem_union;
+    sem_union.val = 1;
+    if (semctl(sem_id, 0, SETVAL, sem_union) == -1)
+        return(0);
+    return(1);
+}
+
+/* del_semvalue remove o semaforo com identificador ID. Tambem usa semctl, mas
+   o comando IPC_RMID remove o semaforo. */
+
+static void del_semvalue(void) {
+    union semun sem_union;
+
+    if (semctl(sem_id, 0, IPC_RMID, sem_union) == -1)
+        fprintf(stderr, "Falha ao remover o semaforo\n");
+}
+
+/* semaphore_p muda o semaforo para -1 (esperando). */
+
+static int semaphore_p(void)
+{
+    struct sembuf sem_b;
+
+    sem_b.sem_num = 0;
+    sem_b.sem_op = -1; /* P() */
+    sem_b.sem_flg = SEM_UNDO;
+    if (semop(sem_id, &sem_b, 1) == -1) {
+        fprintf(stderr, "semaphore_p falhou\n");
+        return(0);
+    }
+    return(1);
+}
+
+/* semaphore_v torna o semaforo disponivel, atribuindo 1 para a parte sem_op do buffer
+   (estrutura sembuf),
+*/
+
+static int semaphore_v(void)
+{
+    struct sembuf sem_b;
+
+    sem_b.sem_num = 0;
+    sem_b.sem_op = 1; /* V() */
+    sem_b.sem_flg = SEM_UNDO;
+    if (semop(sem_id, &sem_b, 1) == -1) {
+        fprintf(stderr, "semaphore_v falhou\n");
+        return(0);
+    }
+    return(1);
 }
 
 #pragma clang diagnostic pop
